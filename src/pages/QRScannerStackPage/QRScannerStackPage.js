0a8359a302a23016000801a35dc362640977da24
Maksat Kuanysh <boktaban@gmail.com>
2023-05-14 12:33:28 +0600

    feat: qr scanner stack page

--- src/pages/QRScannerStackPage/QRScannerStackPage.js
+++ src/pages/QRScannerStackPage/QRScannerStackPage.js
@@ -1,26 +1,36 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Appbar, Button, useTheme } from 'react-native-paper'
+import { useTheme } from 'react-native-paper'
 import { QRScannerPage } from '../QRScannerPage/QRScannerPage'
 import { QRScanEditPage } from '../QRScanEditPage/QRScanEditPage'
+import { useSelector } from 'react-redux'
 
-export const QRScannerStackPage = () => {
+export const QRScannerStackPage = ({ navigation, route }) => {
 	const theme = useTheme()
+	const token = useSelector(state => state.auth.token)
 
 	const Stack = createNativeStackNavigator()
 
 	const stackBaseStyles = {
 		headerTitleStyle: { color: theme.colors.primary },
 		headerStyle: { backgroundColor: theme.colors.background },
-		headerTintColor: theme.colors.primary
+		headerTintColor: theme.colors.primary,
 	}
 
+	if (!token) {
+		alert('Please, authorize first.')
+		navigation.navigate('Signin')
+	}
 	return (
-		<Stack.Navigator>
-			<Stack.Screen name={'QRScannerPage'} component={QRScannerPage} options={{
-				headerShown: false,
-			}}/>
+		<Stack.Navigator initialRouteName={'QRScannerPage'}>
+			<Stack.Screen
+				name={'QRScannerPage'}
+				component={({ navigation }) => <QRScannerPage navigation={navigation}/>}
+				options={{
+					headerShown: false,
+				}}
+			/>
 			<Stack.Screen name={'QRScanEditPage'} component={QRScanEditPage} options={({ route, navigation }) => ({
-				title: route.params.url,
+				title: route.params?.url,
 				...stackBaseStyles,
 			})}/>
 			{/*<Stack.Screen name={'Product'} component={ProductPage} options={({ route, navigation }) => ({*/}
